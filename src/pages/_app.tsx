9237c125db3d2490305b6562168d6bf470184ae1
Nikola Leško <nlesko94@gmail.com>
2022-10-17 07:24:11 +0200

    Add navbar container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const description =
+    "Nikola Leško - Web developer";
+  const title = "Nikola Leško - Web developer";
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        {/* <meta property="og:image" content="" />
+        <meta name="twitter:image" content="" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#000000" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
